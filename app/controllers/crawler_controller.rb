02dff915beb13a5a06999003f2b26300c8b5833c
Jw-Bluayer <wjddn0728@naver.com>
2018-01-19 22:31:51 +0900

    crawler using nokogiri gem

---
+++ app/controllers/crawler_controller.rb
@@ -0,0 +1,12 @@
+require 'open-uri'
+class CrawlerController < ApplicationController
+  def index
+  	# mecahnize, selenium이 크롤링할 때 많이 씀.
+	url = "http://finance.naver.com/marketindex/?tabSel=exchange#tab_section"
+  	doc = Nokogiri::HTML(open(url), nil, 'euc-kr')
+  	currency = doc.css('div.head_info > span.value')
+  	@new_currency = currency.map { |cur| cur.text }
+  	currency_name = doc.css('h3.h_lst')
+  	@new_currency_name = currency_name.map { |cur_n| cur_n.text }
+  end
+end
